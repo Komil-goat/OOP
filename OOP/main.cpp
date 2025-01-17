c5de0ff3aa14fbc913f38d7cd89478ec2c635f0b
Komil-goat <komilkadirov340@gmail.com>
2025-01-17 18:07:24 +0500

    Initial Commit

---
+++ OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Komil Kodirov on 17/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
