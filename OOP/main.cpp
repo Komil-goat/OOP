5ae344a9e0387a7d49b8ad69b467c7f35be4f04b
Komil-goat <komilkadirov340@gmail.com>
2025-01-23 15:03:59 +0500

    commit

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,14 +1,31 @@
-//
-//  main.cpp
-//  OOP
-//
-//  Created by Komil Kodirov on 17/01/25.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main() {
+    int a;
+    cin>>a;
+    vector <int> vec;
+    vector <int> vec1;
+    int j=a,digit;
+    while(j!=0){
+        digit=j%10;
+        j/=10;
+        vec.push_back(digit);
+    }
+    for(int k=int(vec.size())-1; k>=0; k--){
+        vec1.push_back(vec[k]);
+    }
+    int l=0;
+    for(int i=0; i<vec.size(); i++){
+        if(vec[i]==vec1[i]){
+            l++;
+        }
+    }
+    if(l==vec.size()){
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
 }
