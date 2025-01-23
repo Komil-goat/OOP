b0d4c85f3e8d64396ecc9723c3419549b6a61eaa
Komil-goat <komilkadirov340@gmail.com>
2025-01-23 18:32:23 +0500

    guess game

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <time.h>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
-
 int main() {
-    int a;
-    cin>>a;
-    vector <int> vec;
-    vector <int> vec1;
-    int j=a,digit;
-    while(j!=0){
-        digit=j%10;
-        j/=10;
-        vec.push_back(digit);
-    }
-    for(int k=int(vec.size())-1; k>=0; k--){
-        vec1.push_back(vec[k]);
-    }
-    int l=0;
-    for(int i=0; i<vec.size(); i++){
-        if(vec[i]==vec1[i]){
-            l++;
+    srand(int(time(NULL)));
+    int x;
+    cin>>x;
+    int g = 1+rand()%15;
+        while(x!=g){
+            cout<<"NO. Want to continue ?"<<"\n";
+            string d,y;
+            cin>>d;
+            y.resize(d.size());
+            for(int i=0; i<d.size(); i++){
+                y[i]=char(tolower(d[i]));
+            }
+            if(y=="yes"){
+                int l;
+                cin>>l;
+                if(l==g){
+                    cout<<"YOU WON!"<<endl;
+                    break;
+                }
+            } else {
+                break;
+            }
         }
-    }
-    if(l==vec.size()){
-        cout<<"YES"<<endl;
-    } else {
-        cout<<"NO"<<endl;
-    }
 }
